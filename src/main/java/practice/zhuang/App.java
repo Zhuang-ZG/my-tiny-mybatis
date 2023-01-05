33ffe4c13fa8e856e797f72ac39bca78cefebc0c
ZhuangZG <18270806346@163.com>
2023-01-05 19:03:28 +0800

    0 initialize project...

---
+++ src/main/java/practice/zhuang/App.java
@@ -0,0 +1,16 @@
+package practice.zhuang;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author: ZhuangZG
+ * @date 2023/1/5 15:32
+ * @Description:
+ */
+@Slf4j
+public class App {
+
+    public static void main(String[] args) {
+        log.info("hello world");
+    }
+}
